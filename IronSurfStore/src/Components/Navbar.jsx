ff7d512f84867971032dd90d5d054069f2d4c17f
Orlando-Pedrazzoli <pedrazzoliorlando@gmail.com>
2023-12-20 16:28:45 +0000

    added button add to cart and css style

--- IronSurfStore/src/Components/Navbar.jsx
+++ IronSurfStore/src/Components/Navbar.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 function Navbar() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+
+    console.log('search:', searchTerm);
+  };
+
   return (
     <nav className='navbar'>
       <Link to='/' className='nav-logo'>
         <img className='logo' src='/src/assets/img/logo white.png' alt='Logo' />
       </Link>
+
+      <form onSubmit={handleSearchSubmit} className='search-form'>
+        <input
+          type='text'
+          placeholder='Search products...'
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className='search-input'
+        />
+        <button type='submit' className='search-button'>
+          <img src='/src/assets/img/search-icon.png' alt='search-icon' />
+        </button>
+      </form>
       <Link to='/products' className='nav-links'>
         Products
       </Link>
--- IronSurfStore/src/Pages/HomePage.jsx
+++ IronSurfStore/src/Pages/HomePage.jsx
@@ -2,68 +2,170 @@ function HomePage() {
   return (
     <div>
       <div className='banner'>
-        <img src='/src/assets/img/banner.jpg' alt='Banner' />
+        <img src='/src/assets/img/banner-2.jpg' alt='Banner' />
         <div className='banner-text'>ACCEPT NOTHING LESS</div>
+
+        <div className='social-links'>
+          <a
+            href='https://facebook.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Facebook
+          </a>
+          <a
+            href='https://instagram.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Instagram
+          </a>
+          <a
+            href='https://twitter.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Twitter
+          </a>
+        </div>
       </div>
 
       <div className='new-products'>
         <h1>NEW PRODUCTS</h1>
-
         <div className='image-gallery'>
-          <div className='image-container'>
-            <img src='/src/assets/img/IMG_6900.jpeg' alt='' />
-            <p>Descrição do Produto 1</p>
-          </div>
-          <div className='image-container'>
-            <img src='/src/assets/img/IMG_6901.jpeg' alt='' />
-            <p>Descrição do Produto 2</p>
-          </div>
-          <div className='image-container'>
-            <img src='/src/assets/img/IMG_6902.jpeg' alt='' />
-            <p>Descrição do Produto 3</p>
-          </div>
-          <div className='image-container'>
-            <img src='/src/assets/img/IMG_6913.jpeg' alt='' />
-            <p>Descrição do Produto 3</p>
-          </div>
-          <hr />
-          <div className='best-sellers'>
-            <h1>BEST SELLERS</h1>
-
-            <div className='image-gallery'>
-              <div className='image-container'>
-                <img src='/src/assets/img/IMG_6900.jpeg' alt='' />
-                <p>Descrição Best Seller 1</p>
-              </div>
-              <div className='image-container'>
-                <img src='/src/assets/img/IMG_6901.jpeg' alt='' />
-                <p>Descrição Best Seller 2</p>
-              </div>
-              <div className='image-container'>
-                <img src='/src/assets/img/IMG_6902.jpeg' alt='' />
-                <p>Descrição Best Seller 2</p>
-              </div>
-              <div className='image-container'>
-                <img src='/src/assets/img/IMG_6913.jpeg' alt='' />
-                <p>Descrição Best Seller 2</p>
-              </div>
-              <hr />
-              <div>
-                <footer className='footer'>
-                  <p>© 2023 Iron Surf Store - Todos os direitos reservados</p>
-                  <p>
-                    <a href='/contato'>Contato</a>
-                  </p>
-                  <p>
-                    <a href='/sobre'>Sobre Nós</a>
-                  </p>
-                </footer>
-              </div>
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6900.jpeg'
+                alt='Descrição do Produto 1'
+              />
+              <p>Descrição do Produto 1</p>
             </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6901.jpeg'
+                alt='Descrição do Produto 2'
+              />
+              <p>Descrição do Produto 2</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6902.jpeg'
+                alt='Descrição do Produto 3'
+              />
+              <p>Descrição do Produto 3</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6913.jpeg'
+                alt='Descrição do Produto 4'
+              />
+              <p>Descrição do Produto 4</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
+      <hr className='hr-div' />
+
+      <div className='best-sellers'>
+        <h1>BEST SELLERS</h1>
+        <div className='image-gallery'>
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6900.jpeg'
+                alt='Descrição Best Seller 1'
+              />
+              <p>Descrição Best Seller 1</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6901.jpeg'
+                alt='Descrição Best Seller 2'
+              />
+              <p>Descrição Best Seller 2</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6902.jpeg'
+                alt='Descrição Best Seller 3'
+              />
+              <p>Descrição Best Seller 3</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+
+          <div className='product-item'>
+            <div className='image-container'>
+              <img
+                src='/src/assets/img/IMG_6913.jpeg'
+                alt='Descrição Best Seller 4'
+              />
+              <p>Descrição Best Seller 4</p>
+            </div>
+            <div className='product-price'>50.00€</div>
+            <button type='button' className='product-button'>
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <hr className='hr-div' />
+
+      <footer className='footer'>
+        <p>© 2023 Iron Surf Store - Todos os direitos reservados</p>
+        <p>
+          <a href='/contato'>Contato</a>
+        </p>
+        <p>
+          <a href='/sobre'>Sobre Nós</a>
+        </p>
+      </footer>
     </div>
   );
 }
+
 export default HomePage;
